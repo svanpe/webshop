b3b962d9815b0a21ac5e857964b7e36f5d71268a
svanpe <svanpe@gmail.com>
2020-12-11 14:44:19 +0100

    forgot to add the file

---
+++ webshop-front/src/main/java/be/svanpe/api/webshopfront/WebshopFrontApplication.java
@@ -0,0 +1,13 @@
+package be.svanpe.api.webshopfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopFrontApplication.class, args);
+	}
+
+}
